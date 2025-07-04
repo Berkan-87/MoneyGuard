ec45910c8a15d78eb2994aa8c205fa9a10ca6c9a
Berkan-87 <sby.notess@gmail.com>
2025-07-05 00:28:15 +0300

    düzenlendi

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
       components: path.resolve(__dirname, './src/components'),
       hooks: path.resolve(__dirname, './src/hooks'), 
       helpers: path.resolve(__dirname, './src/helpers'),
+      base: '/',
     },
   },
 });
