b8598ea6630c9945d080a2bba1164091a48a9953
Berkan-87 <sby.notess@gmail.com>
2026-01-04 22:09:52 +0300

    x2

--- src/components/common/Logo/Logo.jsx
+++ src/components/common/Logo/Logo.jsx
@@ -6,7 +6,7 @@ const Logo = ({ type }) => {
     <div className={clsx(s.logo, type === 'header' && s.headerLogo)}>
       <img
         // style={{ width, height, fontSize }}
-        src="goktas-logo.svg"
+        src="/goktas-logo.svg"
         alt="Göktaş Logo"
       />
       <h2 className={s.textLogo}>Göktaş</h2>
